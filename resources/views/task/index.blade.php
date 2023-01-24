3fcf786391793662bb5692c17e26bad5aa300d8f
Daniel Ramirez <danielsinsama@gmail.com>
2023-01-23 23:54:43 -0500

    Mejorando vista de index

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -2,51 +2,61 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <h1>
-            Listado de  tareas
-        </h1>
+    
+    <div class="d-flex justify-content-between align-items-center">
+    <h1 class="h1 mt-3 mb-4">
+        Listado de  tareas
+    </h1>
 
-<table class="table">
-    <!-- Fila   : table row -->
-    <tr>
-        <!-- table data -->
-        <td>   
-            Id
-        </td>
-        <td>   
-            Nombre
-        </td>
-        <td>   
-            Descripcion
-        </td>
-        <td>   
-            Score
-        </td>
-        <td>   
-            Editar
-        </td>
-        <td>   
-            Eliminar
-        </td>
-    </tr>
-    @foreach($tasks as $task)
-        <tr>
-            <td>{{$task->id}}</td>
-            <td>{{$task->name}}</td>
-            <td>{{$task->description}}</td>
-            <td>{{$task->score}}</td>
-            <td><a  href="{{url('tasks'.'/'.$task->id.'/'.'edit')}}" class="btn btn-dark">Editar</a></td>
-            <td>   
-                <form method="POST" action="{{url('tasks'.'/'.$task->id)}}">
-                    @csrf
-                    @method('delete')
-                    <button class="btn btn-danger" type="submit">Eliminar</button>
-                </form>
-            </td>
-        </tr>
-    @endforeach
-</table>
+    <p class=""><a class="btn btn-success" href="{{url('tasks'.'/'.'create')}}">Nueva tarea</a></p>
+    </div>
+
+
+    <div class="row justify-content-center">
+        <table class="table table-bordered">
+        <!-- Fila   : table row -->
+        <thead class="table-info">
+            <tr>
+                <!-- table data -->
+                <th>   
+                    Id
+                </th>
+                <th>   
+                    Nombre
+                </th>
+                <th>   
+                    Descripcion
+                </th>
+                <th class="text-center">   
+                    Score
+                </th>
+                <th class="text-center">   
+                    Editar
+                </th>
+                <th class="text-center">   
+                    Eliminar
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($tasks as $task)
+                <tr>
+                    <td>{{$task->id}}</td>
+                    <td>{{$task->name}}</td>
+                    <td>{{$task->description}}</td>
+                    <td class="text-center">{{$task->score}}</td>
+                    <td class="text-center"><a  href="{{url('tasks'.'/'.$task->id.'/'.'edit')}}" class="btn btn-dark">Editar</a></td>
+                    <td class="text-center">   
+                        <form method="POST" action="{{url('tasks'.'/'.$task->id)}}">
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-danger" type="submit">Eliminar</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+        </table>
     </div>
 </div>
 @endsection
